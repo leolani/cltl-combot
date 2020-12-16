c8f1ebf3ee9a751be0cc34813286857cdc28ecf6
Thomas Baier <tbaier@math.bme.hu>
2020-12-16 15:58:24 +0100

    Use dataclasses and Python3 type hints in event API

--- cltl/combot/infra/event/api.py
+++ cltl/combot/infra/event/api.py
@@ -1,5 +1,6 @@
-import time
-import uuid
+from dataclasses import dataclass
+
+from typing import TypeVar, Generic, Optional, Iterable, Callable
 
 from cltl.combot.infra.di_container import DIContainer
 
@@ -8,14 +9,34 @@ class TopicError(ValueError):
     pass
 
 
-class EventBusContainer(DIContainer):
-    @property
-    def event_bus(self):
-        # type: () -> EventBus
-        raise ValueError("No EventBus configured")
+@dataclass
+class EventMetadata:
+    timestamp: int
+    offset: int
+    topic: str
+
+    def with_(self, timestamp: int = None, offset: int = None, topic: str = None) -> Optional["EventMetadata"]:
+        new_timestamp = timestamp if timestamp is not None else self.timestamp
+        new_offset = offset if offset is not None else self.offset
+        new_topic = topic if topic is not None else self.topic
+        return EventMetadata(new_timestamp, new_offset, new_topic)
 
 
-class EventBus(object):
+T = TypeVar("T")
+@dataclass
+class Event(Generic[T]):
+    id: str
+    payload: T
+    metadata: EventMetadata
+
+    def with_topic(self, topic: str) -> Optional["Event"]:
+        return Event(self.id, self.payload, self.metadata.with_(topic=topic))
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+
+class EventBus:
     """
     Supports publishing of and subscribing to events based on topics.
 
@@ -25,73 +46,24 @@ class EventBus(object):
     subscribed to a topic.
     """
 
-    def publish(self, topic, event):
+    def publish(self, topic: str, event: Event) -> None:
         raise NotImplementedError()
 
-    def subscribe(self, topic, handler):
+    def subscribe(self, topic, handler: Callable[[Event], None]) -> None:
         raise NotImplementedError()
 
-    def unsubscribe(self, topic, handler=None):
-        raise NotImplementedError()
-
-    @property
-    def topics(self):
+    def unsubscribe(self, topic: str, handler: Callable[[Event], None] = None) -> None:
         raise NotImplementedError()
 
     @property
-    def has_topic(self, topic):
+    def topics(self) -> Iterable[str]:
+        raise NotImplementedError()
+
+    def has_topic(self, topic: str) -> bool:
         return topic in self.topics
 
 
-class Event(object):
-    def __init__(self, payload, metadata=None, id=None):
-        self._id = id if id else str(uuid.uuid4())
-        self._payload = payload
-        self._metadata = metadata if metadata else EventMetadata(timestamp=time.time())
-
-    def with_topic(self, topic):
-        # type: (str) -> Event
-        return Event(self._payload, self._metadata.with_(topic=topic), id=self._id)
-
+class EventBusContainer(DIContainer):
     @property
-    def metadata(self):
-        # type: () -> EventMetadata
-        return self._metadata
-
-    @property
-    def payload(self):
-        # type: () -> object
-        return self._payload
-
-    def __eq__(self, other):
-        return self._id == other._id
-
-
-class EventMetadata(object):
-    def __init__(self, timestamp=None, offset=None, topic=None):
-        # type: (int, int, str) -> None
-        self._timestamp = timestamp
-        self._offset = offset
-        self._topic = topic
-
-    def with_(self, timestamp=None, offset=None, topic=None):
-        # type: (int, str) -> None
-        new_timestamp = offset if offset is not None else self._timestamp
-        new_offset = offset if offset is not None else self._offset
-        new_topic = topic if topic is not None else self._topic
-        return EventMetadata(new_timestamp, new_offset, new_topic)
-
-    @property
-    def topic(self):
-        # type: () -> str
-        return self._topic
-
-    @property
-    def timestamp(self):
-        # type: () -> int
-        return self._timestamp
-
-    @property
-    def offset(self):
-        # type: () -> int
-        return self._offset
+    def event_bus(self) -> EventBus:
+        raise ValueError("No EventBus configured")
