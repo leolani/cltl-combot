b6f5945c1c79e1aaadd5d8b15a3bbeb373d9866a
Selene Baez <s.baezsantamaria@vu.nl>
2022-05-12 22:51:00 +0200

    add language commons

---
+++ src/cltl/commons/language_helpers.py
@@ -0,0 +1,134 @@
+import importlib.resources
+import json
+
+with importlib.resources.open_text("cltl.commons.language_data", "lexicon.json") as file:
+    lexicon = json.load(file)
+
+with importlib.resources.open_text("cltl.commons.language_data", "cfg.txt") as file:
+    cfg = file.read()
+
+
+def lexicon_lookup(word, typ=None):
+    """
+    Look up and return features of a given word in the lexicon.
+    :param word: word which we're looking up
+    :param typ: type of word, if type is category then returns the lexicon entry and the word type
+    :return: lexicon entry of the word
+    """
+
+    # Define pronoun categories.
+    pronouns = lexicon["pronouns"]
+    subject_pros = pronouns["subject"]
+    object_pros = pronouns["object"]
+    possessive_pros = pronouns["possessive"]
+    dep_possessives = possessive_pros["dependent"]
+    indep_possessives = possessive_pros["independent"]
+    reflexive_pros = pronouns["reflexive"]
+    indefinite_pros = pronouns["indefinite"]
+    indefinite_person = indefinite_pros["person"]
+    indefinite_place = indefinite_pros["place"]
+    indefinite_thing = indefinite_pros["thing"]
+
+    # Define verbal categories.
+    verbs = lexicon["verbs"]
+    to_be = verbs["to be"]
+    aux_verbs = verbs["auxiliaries"]
+    have = aux_verbs['have']
+    to_do = aux_verbs["to do"]
+    modals = aux_verbs["modals"]
+    lexicals = verbs["lexical verbs"]
+
+    # Define determiner categories.
+    determiners = lexicon["determiners"]
+    articles = determiners["articles"]
+    demonstratives = determiners["demonstratives"]
+    possessive_dets = determiners["possessives"]
+    quantifiers = determiners["quantifiers"]
+    wh_dets = determiners["wh-determiners"]
+    numerals = determiners["numerals"]
+    cardinals = numerals["cardinals"]
+    ordinals = numerals["ordinals"]
+    s_genitive = determiners["s-genitive"]
+
+    # Define conjunction categories.
+    conjunctions = lexicon["conjunctions"]
+    coordinators = conjunctions["coordinating"]
+    subordinators = conjunctions["subordinating"]
+
+    # Define a question word category.
+    question_words = lexicon["question words"]
+
+    # Define a kinship category.
+    kinship = lexicon["kinship"]
+
+    if typ == 'verb':
+        categories = [to_be,
+                      to_do,
+                      have,
+                      modals,
+                      lexicals]
+
+    elif typ == 'pos':
+        categories = [dep_possessives]
+
+    elif typ == 'to_be':
+        categories = [to_be]
+
+    elif typ == 'aux':
+        categories = [to_do, to_be, have]
+
+    elif typ == 'modal':
+        categories = [modals]
+
+    elif typ == 'pronouns':
+        categories = [subject_pros,
+                      object_pros,
+                      dep_possessives,
+                      indep_possessives,
+                      reflexive_pros,
+                      indefinite_person,
+                      indefinite_place,
+                      indefinite_thing]
+    elif typ == 'lexical':
+        categories = [lexicals]
+    elif typ == 'kinship':
+        categories = [kinship]
+    elif typ == 'det':
+        categories = [articles, demonstratives, possessive_dets, possessive_pros, cardinals, ordinals]
+    else:
+        categories = [subject_pros,
+                      object_pros,
+                      dep_possessives,
+                      indep_possessives,
+                      reflexive_pros,
+                      indefinite_person,
+                      indefinite_place,
+                      indefinite_thing,
+                      to_be,
+                      to_do,
+                      have,
+                      modals,
+                      lexicals,
+                      articles,
+                      demonstratives,
+                      possessive_dets,
+                      quantifiers,
+                      wh_dets,
+                      cardinals,
+                      ordinals,
+                      s_genitive,
+                      coordinators,
+                      subordinators,
+                      question_words,
+                      kinship]
+
+    for category in categories:
+        for item in category:
+            if word == item:
+                if typ == 'category':
+                    return category, category[item]
+                return category[item]
+    return None
+
+
+
