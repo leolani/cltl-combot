0429d43052a8793776678d2e94c4e75cf6b614e3
Selene Baez Sanamaria <s.baezsantamaria@vu.nl>
2022-09-28 19:14:33 +0200

    transformation to enum classes

--- src/cltl/commons/discrete.py
+++ src/cltl/commons/discrete.py
@@ -7,11 +7,75 @@ class Certainty(enum.Enum):
     PROBABLE = 2
     CERTAIN = 3
 
+    @staticmethod
+    def from_str(label):
+        try:
+            return Certainty[label.upper()]
+        except:
+            return Certainty.UNDERSPECIFIED
+
+    @staticmethod
+    def from_float(numeric):
+        if numeric > .90:
+            return Certainty.CERTAIN
+        elif numeric >= .50:
+            return Certainty.PROBABLE
+        elif numeric > 0:
+            return Certainty.POSSIBLE
+        else:
+            return Certainty.UNDERSPECIFIED
+
+    @staticmethod
+    def as_enum(value):
+        # Convert to enum
+        if value is None:
+            return Certainty.UNDERSPECIFIED
+        elif isinstance(value, Certainty):
+            return value
+        elif isinstance(value, int) or isinstance(value, float):
+            return Certainty.from_float(value)
+        elif isinstance(value, str) and value.isnumeric():
+            return Certainty.from_float(float(value))
+        elif isinstance(value, str):
+            return Certainty.from_str(value)
+        else:
+            return Certainty.UNDERSPECIFIED
+
 
 class Polarity(enum.Enum):
     UNDERSPECIFIED = 0
     NEGATIVE = -1
     POSITIVE = 1
+
+    @staticmethod
+    def from_str(label):
+        try:
+            return Polarity[label.upper()]
+        except:
+            return Polarity.UNDERSPECIFIED
+
+    @staticmethod
+    def from_float(numeric):
+        if numeric >= 0:
+            return Polarity.POSITIVE
+        elif numeric < 0:
+            return Polarity.NEGATIVE
+
+    @staticmethod
+    def as_enum(value):
+        # Convert to enum
+        if value is None:
+            return Polarity.UNDERSPECIFIED
+        elif isinstance(value, Polarity):
+            return value
+        elif isinstance(value, int) or isinstance(value, float):
+            return Polarity.from_float(value)
+        elif isinstance(value, str) and value.isnumeric():
+            return Polarity.from_float(float(value))
+        elif isinstance(value, str):
+            return Polarity.from_str(value)
+        else:
+            return Polarity.UNDERSPECIFIED
 
 
 class Sentiment(enum.Enum):
@@ -19,6 +83,38 @@ class Sentiment(enum.Enum):
     NEGATIVE = -1
     POSITIVE = 1
     NEUTRAL = 2
+
+    @staticmethod
+    def from_str(label):
+        try:
+            return Sentiment[label.upper()]
+        except:
+            return Sentiment.UNDERSPECIFIED
+
+    @staticmethod
+    def from_float(numeric):
+        if numeric > 0:
+            return Sentiment.POSITIVE
+        elif numeric < 0:
+            return Sentiment.NEGATIVE
+        elif numeric == 0:
+            return Sentiment.NEUTRAL
+
+    @staticmethod
+    def as_enum(value):
+        # Convert to enum
+        if value is None:
+            return Sentiment.UNDERSPECIFIED
+        elif isinstance(value, Sentiment):
+            return value
+        elif isinstance(value, int) or isinstance(value, float):
+            return Sentiment.from_float(value)
+        elif isinstance(value, str) and value.isnumeric():
+            return Sentiment.from_float(float(value))
+        elif isinstance(value, str):
+            return Sentiment.from_str(value)
+        else:
+            return Sentiment.UNDERSPECIFIED
 
 
 class Emotion(enum.Enum):
@@ -30,6 +126,29 @@ class Emotion(enum.Enum):
     SADNESS = 5
     SURPRISE = 6
     NEUTRAL = 7
+
+    @staticmethod
+    def from_str(label):
+        try:
+            return Emotion[label.upper()]
+        except:
+            return Emotion.UNDERSPECIFIED
+
+    @staticmethod
+    def as_enum(value):
+        # Convert to enum
+        if value is None:
+            return Emotion.UNDERSPECIFIED
+        elif isinstance(value, Emotion):
+            return value
+        # elif isinstance(value, int) or isinstance(value, float):
+        #     return Emotion.from_float(value)
+        # elif isinstance(value, str) and value.isnumeric():
+        #     return Emotion.from_float(float(value))
+        elif isinstance(value, str):
+            return Emotion.from_str(value)
+        else:
+            return Emotion.UNDERSPECIFIED
 
 
 class Time(enum.Enum):
